e51f6a87f61f841304f1b0f4320bf38a9212eb8c
Alif Arkan <cotobakartech@gmail.com>
2026-06-22 23:51:52 +0700

    initial commit

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     serve(
         app, 
         host='0.0.0.0', 
-        port=8080, 
+        port=80, 
         threads=12,              # Menangani hingga 12 proses sekaligus
         connection_limit=200,    # Batas koneksi masuk
         channel_timeout=30,      # Timeout untuk membebaskan koneksi yang menggantung
